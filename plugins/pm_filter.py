1d10b1b0030eda50c3b99d46538c8a38a54fff38
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-02 12:51:08 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -619,7 +619,7 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'files_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text=f"📤'☘𝐈𝐦𝐝𝐛☘', url="https://imdb.com'
+                    text=f"📤'☘𝐈𝐦𝐝𝐛☘', url="https://imdb.com',
                     
             ]
             for file in files
