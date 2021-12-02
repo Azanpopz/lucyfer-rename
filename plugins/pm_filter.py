fc1fe72c031b3a06a30d13a58b4c877c16ffd7e9
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-02 14:47:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -615,6 +615,7 @@ async def auto_filter(client, msg, spoll=False):
                 
                 )
            ],
+            [
                 InlineKeyboardButton(
                     text="🎨JOIN", url='https://t.me/bigmoviesworld'
                 
