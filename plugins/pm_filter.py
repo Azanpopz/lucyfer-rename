6615240539dae49c1d556d0d06e8ee6eb05fd251
NasraniPro <95268232+NasraniPro@users.noreply.github.com>
2021-12-02 13:38:45 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -619,7 +619,9 @@ async def auto_filter(client, msg, spoll=False):
                     callback_data=f'files_#{file.file_id}',
                 ),
                 InlineKeyboardButton(
-                    text="CANCEL", callback_data="close_data'
+                    text="CANCEL", callback_data="close_data"
+                InlineKeyboardButton(
+                    text="CANCE😜L", callback_data="close_data"
                 ),
                     
             ]
