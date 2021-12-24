a26ed83f81a48867d155fa2d33d057386dc910ea
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-24 14:20:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -19,7 +19,7 @@ class Bot(Client):
     def __init__(self):
         super().__init__(
             session_name=SESSION,
-            API_KEY=API_KEY,
+            api_key=API_KEY,
             api_id=API_ID,
             api_hash=API_HASH,
             bot_token=BOT_TOKEN,
