997088fd8898fe6d91b629ed2161197bb5942658
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-24 21:35:56 +0530

    Update antiflood.py

--- plugins/antiflood.py
+++ plugins/antiflood.py
@@ -13,9 +13,9 @@ from pyrobot import (
     COMMAND_HAND_LER,
     DATABASE_URI
 )
-from pyrobot.helper_functions.admin_check import admin_check
-from pyrobot.helper_functions.cust_p_filters import f_onw_fliter
-if DB_URI is not None:
+from plugins.helper_functions.admin_check import admin_check
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+if DATABASE_URI is not None:
     import pyrobot.helper_functions.sql_helpers.antiflood_sql as sql
     CHAT_FLOOD = sql.__load_flood_settings()
 
