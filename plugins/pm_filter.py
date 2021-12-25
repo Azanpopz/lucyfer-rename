3b0d3402b5a5554c0d75a1331d3ac4a6cad0876f
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-25 20:39:29 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -496,7 +496,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.SOURCE_TXT,
+            text=script.TELEGRAPH_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
         )
