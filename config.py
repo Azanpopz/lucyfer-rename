f9a10cce4f29035a23bc8a29f93d976864071da3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-25 16:38:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,19 @@ import os
 import re
 from youtube_dl import YoutubeDL
 
+
+import  os
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+APP_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+
+youtube_next_fetch = 0  # time in minute
+
+
+EDIT_TIME = 5
+
+
 class Config:
     API_ID = int(os.environ.get("APP_ID", ""))
     API_HASH = os.environ.get("API_HASH", "")
