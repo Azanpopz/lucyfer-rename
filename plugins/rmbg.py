cf1e0fca5898facb45151abbb259740c7f8c48c0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-26 11:47:09 +0530

    Update rmbg.py

--- plugins/rmbg.py
+++ plugins/rmbg.py
@@ -19,7 +19,7 @@ UNSCREEN_API = os.environ.get("UNSCREEN_API", "")
 
 
 
-@Bot.on_message(filters.private & (filters.photo | filters.video | filters.document))
+@Client.on_message(filters.private & (filters.photo | filters.video | filters.document))
 async def remove_background(bot, update):
     if not REMOVEBG_API:
         await update.reply_text(
