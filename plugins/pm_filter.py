ef24d58c30f26d6922666e7cc25eb48df642cdbd
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-26 12:51:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -856,6 +856,7 @@ async def advantage_spell_chok(msg):
                             ],
                             [
                                 InlineKeyboardButton('🧩𝐆𝐨𝐨𝐠𝐥𝐞🧩', url="https://google.com"),
+                                InlineKeyboardButton('🧩𝐆𝐨𝐨𝐠𝐥𝐞🧩', switch_inline_query_current_chat=update.text"),
                                 InlineKeyboardButton('☘𝐈𝐦𝐝𝐛☘', url="https://imdb.com")
                             ]                            
                         ]
