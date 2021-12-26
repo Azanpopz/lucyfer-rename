c50d6a5eec73e5020b8315f17b7cb525d6e65a34
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-26 11:07:01 +0530

    Create Filetolink.py

---
+++ plugins/Filetolink.py
@@ -0,0 +1,28 @@
+import telebot
+import time
+import pyshorteners
+import os
+
+bot = telebot.TeleBot(token=os.getenv('TG_BOT_TOKEN'))
+
+def short(url):
+    return pyshorteners.Shortener().tinyurl.short(url)
+
+
+@bot.message_handler(content_types=['photo', 'video', 'audio', 'document'])
+def file_sent(message):
+    try:
+        bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
+    except AttributeError:
+        try:
+            bot.send_message(message.chat.id, short(bot.get_file_url(message.photo[0].file_id)))
+        except AttributeError:
+            try:
+                bot.send_message(message.chat.id, short(bot.get_file_url(message.audio.file_id)))
+            except AttributeError:
+                try:
+                    bot.send_message(message.chat.id, short(bot.get_file_url(message.video.file_id)))
+                except AttributeError:
+                    pass
+
+
