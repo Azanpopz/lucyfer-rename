f91827e50e33b4c62de9e370b06d14964b8d0699
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-26 11:27:43 +0530

    Update filetolink.py

--- plugins/filetolink.py
+++ plugins/filetolink.py
@@ -17,8 +17,9 @@ def short(url):
     return pyshorteners.Shortener().tinyurl.short(url)
 
 
-@bot.on_message_handler(content_types=['photo', 'video', 'audio', 'document'])
-def file_sent(message):
+@@Client.on_message(filters.regex(r'https?://[^\s]+') & filters.private)
+async def link_handler(bot, message):
+    link = message.matches[0].group(0)
     try:
         bot.send_message(message.chat.id, short(bot.get_file_url(message.document.file_id)))
     except AttributeError:
