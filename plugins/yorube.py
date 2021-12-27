b47b9b12937886a07806b677235321886c3c7750
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-27 22:14:39 +0530

    Update yorube.py

--- plugins/yorube.py
+++ plugins/yorube.py
@@ -13,12 +13,11 @@ BOT_TOKEN = os.environ.get('BOT_TOKEN')
 
 
 
-@Client.on_message(
-    filters.command("vid") 
-):
-async def vid(client, message):    
+@Client.on_message(filters.command(["vid"]))
+async def vid(bot, update):
+    await update.reply_text(
     results = VideosSearch(update.vid, limit=50).result()
-    answers = []
+    answers = [])
     
     for result in results:
         title = result["title"]
