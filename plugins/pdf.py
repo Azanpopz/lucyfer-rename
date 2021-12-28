c90ff5acd3b64ea0d698f15c503648fd62ea7706
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 15:20:50 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -20,7 +20,7 @@ LIST = {}
 
 
 
-@Client.on_message(filters.private & filters.photo)
+@Client.on_message(filters.photo & filters.command(['con']))
 async def pdf(client,message):
  
  if not isinstance(LIST.get(message.from_user.id), list):
@@ -39,20 +39,7 @@ async def pdf(client,message):
  
 
 @Client.on_message(filters.command(['convert']))
-async def done(client,message):
- if not isinstance(LIST.get(message.from_user.id), list):
-   LIST[message.from_user.id] = []
- images = LIST.get(message.from_user.id)
- 
- file_id = str(message.photo.file_id)
- ms = await message.reply_text("Converting to PDF ......")
- file = await client.download_media(file_id)
- 
- image = Image.open(file)
- img = image.convert('RGB')
- LIST[message.from_user.id].append(img)
- await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
- 
+
 
  if isinstance(images, list):
   del LIST[message.from_user.id]
