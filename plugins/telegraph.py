6200404579a9e66b64584e376ebdaa93d783a5d1
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 21:30:56 +0530

    Update telegraph.py

--- plugins/telegraph.py
+++ plugins/telegraph.py
@@ -4,6 +4,7 @@ from pyrogram import Client, filters
 from telegraph import upload_file
 from plugins.helper_functions.cust_p_filters import sudo_filter
 from plugins.helper_functions.get_file_id import get_file_id
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 TMP_DOWNLOAD_DIRECTORY = "./DOWNLOADS/"
@@ -35,10 +36,19 @@ async def telegraph(client, message):
     except Exception as document:
         await message.reply_text(message, text=document)
     else:
-        await message.reply(
-            f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>",
-            disable_web_page_preview=True
+        await message.reply_text(
+            text=f"<b>Link :-</b> <code>https://telegra.ph{response[0]}</code>\n\n<b>Join :-</b> @FayasNoushad",
+        disable_web_page_preview=True,
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(text="Open Link", url=f"https://telegra.ph{response[0]}"),
+                    InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://telegra.ph{response[0]}")
+                ],
+                [InlineKeyboardButton(text="⚙ Join Updates Channel ⚙", url="https://telegram.me/FayasNoushad")]
+            ]
         )
+    )
     finally:
         shutil.rmtree(
             _t,
