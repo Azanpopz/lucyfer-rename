e624b5581bdcb7edb7c0d4d6cec84072ccdf0364
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-29 01:09:42 +0530

    Update link.py

--- plugins/link.py
+++ plugins/link.py
@@ -2,6 +2,8 @@ from os import environ
 import aiohttp
 from pyrogram import Client, filters
 from info import API_ID, API_HASH, API_KEY, BOT_TOKEN
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
