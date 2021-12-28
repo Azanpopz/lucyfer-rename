d6c887cfaaac612dc7df6899dc24305356a6e355
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 20:35:14 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -32,7 +32,7 @@ from motor.motor_asyncio import AsyncIOMotorClient as MongoClient
 from pyrogram import Client
 from pyrogram.types import Message
 from pyromod import listen
-from Python_ARQ import ARQ
+
 from telegraph import Telegraph
 
 is_config = path.exists("config.py")
