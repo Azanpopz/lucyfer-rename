0e1658214dbc5e576b4e97d7ec9b19e1675d96b2
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 09:26:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ import  os
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-REMOVEBG_API = os.environ.get("REMOVEBG_API", "")
+RemoveBG_API = os.environ.get("RemoveBG_API", "")
 
 
 
