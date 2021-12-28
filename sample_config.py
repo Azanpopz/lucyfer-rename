01281b31d0def562833ce8c83c2fd3b070cec0c7
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 09:26:10 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -9,4 +9,4 @@ class Config(object):
     API_HASH = os.environ.get("API_HASH", "")
 
     # Get this api from https://www.remove.bg/b/background-removal-api
-    REMOVEBG_API = os.environ.get("REMOVEBG_API", "")
+    RemoveBG_API = os.environ.get("RemoveBG_API", "")
