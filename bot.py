0a40567e8a143630cbdeb44f9b569f5706f616be
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 09:39:23 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ import os
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
 else:
-    from config import Config  # pylint:disable=import-error
+    from confi import Config  # pylint:disable=import-error
 
 
 
