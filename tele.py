80fca4aa2ed77e57d1adc7e3af09f5f6c20c649a
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 21:19:49 +0530

    Update tele.py

--- tele.py
+++ tele.py
@@ -14,7 +14,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid, UserNotParticipant, UserBannedInChannel
 from pyrogram.errors.exceptions.bad_request_400 import PeerIdInvalid
 from telegraph import upload_file
-from database import Database
+from database import base
 
 
 AUTH_CHANNEL = os.environ.get("AUTH_CHANNEL", "")
