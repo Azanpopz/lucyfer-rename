9a2da3422f6c2b9b2c6dc20134a68778ba66e215
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 15:14:31 +0530

    Update pdf.py

--- plugins/pdf.py
+++ plugins/pdf.py
@@ -42,6 +42,16 @@ async def pdf(client,message):
 async def done(client,message):
  images = LIST.get(message.from_user.id)
 
+ file_id = str(message.photo.file_id)
+ ms = await message.reply_text("Converting to PDF ......")
+ file = await client.download_media(file_id)
+ 
+ image = Image.open(file)
+ img = image.convert('RGB')
+ LIST[message.from_user.id].append(img)
+ await ms.edit(f"{len(LIST[message.from_user.id])} image   Successful created PDF if you want add more image Send me One by one\n\n **if done click here 👉 /convert** ")
+ 
+
  if isinstance(images, list):
   del LIST[message.from_user.id]
  if not images:
