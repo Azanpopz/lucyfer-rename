05811efd0271912afc178396c612a724103eeba8
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-28 09:42:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,20 +3,7 @@ import re
 from youtube_dl import YoutubeDL
 
 
-import os
 
-
-class Config(object):
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-
-    API_ID = int(os.environ.get("API_ID", 12345))
-
-    API_HASH = os.environ.get("API_HASH", "")
-
-    # Get this api from https://www.remove.bg/b/background-removal-api
-    RemoveBG_API = os.environ.get("RemoveBG_API",
-
-import  os
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
 API_ID = int(os.environ.get("API_ID"))
