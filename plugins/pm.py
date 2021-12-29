a4d6e8e13f76ee3d5d10ae88a20e39a192befb69
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-29 21:55:46 +0530

    Update pm.py

--- plugins/pm.py
+++ plugins/pm.py
@@ -12,13 +12,13 @@ else:
 
 @Client.on_message(filters.private & filters.text)
 async def pm_text(bot, message):
-    if message.from_user.id == Config.ADMIN:
+    if message.from_user.id == Config.ADMINS:
         await reply_text(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
     await bot.send_message(
-        chat_id=Config.ADMIN,
+        chat_id=Config.ADMINS,
         text=Presets.PM_TXT_ATT.format(reference_id, info.first_name, message.text),
         parse_mode="html"
     )
@@ -26,13 +26,13 @@ async def pm_text(bot, message):
 
 @Client.on_message(filters.private & filters.media)
 async def pm_media(bot, message):
-    if message.from_user.id == Config.ADMIN:
+    if message.from_user.id == Config.ADMINS:
         await replay_media(bot, message)
         return
     info = await bot.get_users(user_ids=message.from_user.id)
     reference_id = int(message.chat.id)
     await bot.copy_message(
-        chat_id=Config.ADMIN,
+        chat_id=Config.ADMINS,
         from_chat_id=message.chat.id,
         message_id=message.message_id,
         caption=Presets.PM_MED_ATT.format(reference_id, info.first_name),
@@ -40,7 +40,7 @@ async def pm_media(bot, message):
     )
 
 
-@Client.on_message(filters.user(Config.ADMIN) & filters.text)
+@Client.on_message(filters.user(Config.ADMINS) & filters.text)
 async def reply_text(bot, message):
     reference_id = True
     if message.reply_to_message is not None:
@@ -59,7 +59,7 @@ async def reply_text(bot, message):
         )
 
 
-@Client.on_message(filters.user(Config.ADMIN) & filters.media)
+@Client.on_message(filters.user(Config.ADMINS) & filters.media)
 async def replay_media(bot, message):
     reference_id = True
     if message.reply_to_message is not None:
