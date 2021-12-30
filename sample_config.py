9e21bf0df51fb0cf1ce901faeeb9de31aa9bfbaf
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-30 21:06:51 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -4,4 +4,4 @@ class Config(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    ADMIN = int(os.environ.get("ADMIN"))
+    ADMINS = int(os.environ.get("ADMINS"))
