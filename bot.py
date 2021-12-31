553cd2e95f92f561ab68aff7fbb372f89f146519
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-31 12:15:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,22 +1,4 @@
 
-#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
-import os
-import logging
-
-from pyrogram import Client
-
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-logger = logging.getLogger(__name__)
-
-logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
-if bool(os.environ.get("ENV", False)):
-    from sample_config import Config
-else:
-    from config import Config
-
-if __name__ == "__main__":
     
 
 
