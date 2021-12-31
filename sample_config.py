56a7afbc3fbfc0ef066b1692cd66795d5eafcfd0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2021-12-31 12:12:02 +0530

    Create sample_config.py

---
+++ sample_config.py
@@ -0,0 +1,8 @@
+#-------------------------------------- https://github.com/m4mallu/PMChatbot ------------------------------------------#
+import os
+
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN")
+    API_ID = int(os.environ.get("API_ID"))
+    API_HASH = os.environ.get("API_HASH")
+    ADMIN = int(os.environ.get("ADMIN"))
