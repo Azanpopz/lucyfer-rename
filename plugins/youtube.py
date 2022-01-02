536e90060c582b48494c23dd3ebb127b6da9a3fd
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-03 00:11:32 +0530

    Create youtube.py

---
+++ plugins/youtube.py
@@ -0,0 +1,41 @@
+from pyrogram import Client ,filters
+import os
+from py_youtube import Data, Search 
+from pyrogram.types import *
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", ""))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+
+    
+	
+
+
+@Client.on_inline_query()
+async def search_video(client,query):
+	search = []
+	result = Search(query.query.strip()).videos()
+	for i in result:
+		try:
+			title = i["title"]
+			id = i["id"]
+			thumb = i["thumb"][0]
+			data = i["simple_data"]
+		except:
+			pass
+		try:
+			search.append(
+                InlineQueryResultPhoto(
+                    title=title,
+                    description=data,
+                    caption="https://youtu.be/"+id,
+                    photo_url=thumb))
+		
+		except:
+		          pass
+            
+	await query.answer(search)
+	
