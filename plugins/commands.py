a059e68eca6784a3a6a902fd728baf2b4c396135
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-04 00:09:26 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -125,7 +125,7 @@ async def start(client, message):
         return await message.reply(f"<b><a href='https://t.me/NasraniChatGroup'>Thank For Using Me...</a></b>")
 
         await asyncio.sleep(30)      # sleep 30 second
-        await k.delete()
+        await get_batch.delete()
 
 
 
