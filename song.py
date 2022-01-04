8c63a0aef2f41d1e59c607b59beb9ac97b0181db
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-04 22:31:45 +0530

    Update song.py

--- song.py
+++ song.py
@@ -32,7 +32,21 @@ from youtube_dl.utils import (
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery, InlineQuery, InputTextMessageContent
 
 
-   
+class Config(object):
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+    API_ID = int(os.environ.get("API_ID", 12345))
+
+    API_HASH = os.environ.get("API_HASH", "")   
+
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+
+API_ID = int(os.environ.get("API_ID", 12345))
+
+API_HASH = os.environ.get("API_HASH", "")
+
+
 
 API_ID = environ.get('API_ID')
 API_HASH = environ.get('API_HASH')
