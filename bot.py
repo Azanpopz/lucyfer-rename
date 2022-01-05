cb3a2f305090e2e5cb8ca785064117ca83fcf894
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-05 11:44:39 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,13 @@
+import os
+import asyncio
+from urllib.parse import urlparse
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
+from pyrogram import Client, filters
+from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
+from youtube_dl import YoutubeDL
+from opencc import OpenCC
 from con import Con
+import wget
     
 
 
