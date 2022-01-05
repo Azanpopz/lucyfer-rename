253ad2e9871147de38e335b9f5a40f7d91fc03cf
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-05 11:46:00 +0530

    Update video.py

--- plugins/video.py
+++ plugins/video.py
@@ -23,6 +23,19 @@ from opencc import OpenCC
 from con import Con
 import wget
 
+ZauteKm = Client(
+   "AnyDL Bot",
+   api_id=Con.API_ID,
+   api_hash=Con.API_HASH,
+   bot_token=Con.BOT_TOKEN,
+)
+
+YTDL_REGEX = (r"^((?:https?:)?\/\/)"
+              r"?((?:www|m)\.)"
+              r"?((?:youtube\.com|youtu\.be|xvideos\.com|pornhub\.com"
+              r"|xhamster\.com|xnxx\.com))"
+              r"(\/)([-a-zA-Z0-9()@:%_\+.~#?&//=]*)([\w\-]+)(\S+)?$")
+s2tw = OpenCC('s2tw.json').convert
 
 
 
