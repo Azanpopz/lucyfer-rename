1f789d5c2f3e38f42b34eb2ba3e0e605055a5caf
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-06 00:19:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,28 @@
 
 from bot import Bot
 
+
+
+
+
+from pyrogram import Client, filters
+
+import youtube_dl
+from youtube_search import YoutubeSearch
+import requests
+
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+import os
+from config import Config
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+
+
+
+
 Bot().run()
