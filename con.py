91319556f5c11327dbe72700c46eaf1dc213f893
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-05 11:41:44 +0530

    Update con.py

--- con.py
+++ con.py
@@ -18,7 +18,7 @@ import os
 class Con(object):
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    API_ID = int(os.environ.get("API_ID", 12345))
 
     API_HASH = os.environ.get("API_HASH", "")
 
