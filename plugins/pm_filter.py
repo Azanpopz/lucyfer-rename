12a3fbe9edb4640ad99e9db477ad317f55da18e3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-01-17 11:00:00 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -100,14 +100,8 @@ async def next_page(bot, query):
             [
                 InlineKeyboardButton(
                     text=f"[📺{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
-                )
-            ],
-              [
-            [
-                InlineKeyboardButton(
-                    text=f"[📺{get_size(file.file_size)}] {file.file_name}", callback_data=f'files#{file.file_id}'
                 ),
-            ]
+            ]  
             for file in files
         ]
     else:
