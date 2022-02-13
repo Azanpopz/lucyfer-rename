eda73e95bccb97c6bd19a010b126c2bc919545ef
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-02-13 20:12:14 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -152,8 +152,8 @@ async def start(client, message):
                     )
                 )
                          
-        await asyncio.sleep(30)
-        await k.delete()
+                    await asyncio.sleep(30)
+                    await k.delete()
             except Exception as err:
                 return await message.reply(f"{str(err)}")
             
