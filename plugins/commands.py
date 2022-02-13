e748218d27df7d8728dcce0d432ead31981ec6b3
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-02-13 19:21:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -150,14 +150,14 @@ async def start(client, message):
                         ]
                     )
                 )
-                      
+                         
          
             except Exception as err:
-                return await message.reply(f"{str(err)}")
-            
+                return k = await message.reply(f"{str(err)}")
+                await asyncio.sleep(30)
+                await k.delete()
         return await message.reply(f"<b><a href='https://t.me/NasraniChatGroup'>Thank For Using Me...</a></b>")
-        await asyncio.sleep(30)
-        await k.delete()
+        
 
 
     files_ = await get_file_details(file_id)
