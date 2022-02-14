3a4ae9b6c356b5e5fb572cdec18647f86a6b2ea0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-02-14 14:00:50 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -942,7 +942,7 @@ async def advantage_spell_chok(msg):
         video= "https://telegra.ph/file/ec5404d035924f1113d8d.mp4",
         caption=f"<b>📍Hello:-നിങ്ങൾ ചോദിച്ച മൂവി വേണമെങ്കിൽ മുകളിലെ വീഡിയോ കണ്ട് അത് പോലെ സ്പെല്ലിങ് തെറ്റാതെ അയക്കുക.😌</b>",
         parse_mode="html",
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
+        reply_markup=InlineKeyboardMarkup(
                         [
                             [
                                 InlineKeyboardButton("Text", "alert")
@@ -954,7 +954,7 @@ async def advantage_spell_chok(msg):
                         ]
                     )
                 )         
-        )
+        
                             
 
         await asyncio.sleep(60)
