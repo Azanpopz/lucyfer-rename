fc1bc04c43ab3251ff549693bfda13b281adf7f0
Azanpopz <89448511+Azanpopz@users.noreply.github.com>
2022-02-14 11:31:41 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,7 +41,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[            
-            InlineKeyboardButton('[button text]', (buttonalert='Your Text)'),            
+            InlineKeyboardButton('[button text]', (buttonalert:'Your Text)'),            
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         if message.from_user.id == ADMIN:
